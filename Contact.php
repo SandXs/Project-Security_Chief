328e046acaa05519a542f520c310071c408d5923
Jack Kartoush <jackkartosh@gmail.com>
2023-11-20 09:38:12 +0100

    adding contact page

---
+++ Contact.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php 
+include './Header.php';
+?>
+
+<script>
+setTimeout(function() {
+    window.location.reload(1);
+}, 5000);
+</script>
+
+
+    <section class="page-container">
+</body>
+</html>
