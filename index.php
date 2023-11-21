3a4b3d3330a09a3b0d8c101d8f55aa818afc9140
KevinScheper <kevin.scheper@hotmail.nl>
2023-11-21 14:41:17 +0100

    Added content to the page

--- index.php
+++ index.php
@@ -45,9 +45,16 @@ setTimeout(function() {
                 </div>
                 <div class="info-text">
                     <h1> Phishing </h1>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                        Maecenas et iaculis risus. Integer rhoncus, ipsum at congue commodo,
-                        purus velit viverra massa, </p>
+                    <p>Phishing is cybercrime waarbij criminelen proberen inloggegevens, creditcardinformatie, pincodes of 
+                        andere persoonlijke gegevens van jou te achterhalen. 
+                   
+                    <h3>Hoe kan ik het voorkomen?</h3>
+                   <ol>
+                    <ul>• Reageer niet op het bericht.</ul>
+                  	<ul>• Meld de phishing bij Fraudehelpdesk </ul>
+                      </ol>
+                  
+                    </p>
                     <button typ="button" class="readBtn-wraper"><a href="#" class="read-btn">Read more</a></button>
                 </div>
             </div>
@@ -57,11 +64,157 @@ setTimeout(function() {
                 </div>
                 <div class="info-text">
                     <h1> Scam </h1>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                        Maecenas et iaculis risus. Integer rhoncus, ipsum at congue commodo,
-                        purus velit viverra massa, </p>
+                    <p>Helaas zullen er altijd mensen zijn die de boel moedwillig oplichten. U betaalt keurig, 
+                        maar ontvangt vervolgens niets. Of u wordt overgehaald om vast uw product op te sturen, 
+                        maar ontvangt nooit een betaling. In beide gevallen bent u waarschijnlijk opgelicht. 
+                   
+                    <h3>Hoe kan ik het voorkomen?</h3>
+                   <ol>
+                    <ul>• Doe onderzoek naar de persoon of naar het bedrijf van waar je het koopt.</ul>
+                  	<ul>• Als je het niet vertrouwd koop het niet. </ul>
+                      </ol>
+                  
+                    </p>
                     <button typ="button" class="readBtn-wraper"><a href="#" class="read-btn">Read more</a></button>
                 </div>
+
+                <div class="info-wraper">
+                <div class="info-foto-container">
+                    <img src="./img/scam.jpg" alt="">
+                </div>
+                <div class="info-text">
+                    <h1> Malware </h1>
+                    <p>Malware kan computers en apparaten op verschillende manieren infecteren en bestaat in verschillende vormen, 
+                        zoals virussen, wormen, trojans en spyware. 
+                   
+                    <h3>Hoe kan ik het voorkomen?</h3>
+                   <ol>
+                    <ul>• Wees op je hoeden.</ul>
+                  	<ul>• Gebruik Beveiligingssoftware. </ul>
+                      </ol>
+                  
+                    </p>
+                    <button typ="button" class="readBtn-wraper"><a href="#" class="read-btn">Read more</a></button>
+                </div>
+
+                <div class="info-wraper">
+                <div class="info-foto-container">
+                    <img src="./img/scam.jpg" alt="">
+                </div>
+                <div class="info-text">
+                    <h1> Hacking </h1>
+                    <p> Hacking betekent dat iemand toegang probeert te krijgen tot de gegevens van iemand anders, via het gebruik van 
+                        computers en het netwerk. Het is strafbaar als dit gebeurt zonder toestemming van de eigenaar van die gegevens.
+                   
+                    <h3>Hoe kan ik het voorkomen?</h3>
+                   <ol>
+                    <ul>•Gebruik sterke wachtwoorden</ul>
+                  	<ul>•Vermijd dubieuze websites </ul>
+                      </ol>
+                  
+                    </p>
+                    <button typ="button" class="readBtn-wraper"><a href="#" class="read-btn">Read more</a></button>
+                </div>
+                
+                <div class="info-wraper">
+                <div class="info-foto-container">
+                    <img src="./img/scam.jpg" alt="">
+                </div>
+                <div class="info-text">
+                    <h1> Ransomware </h1>
+                    <p>Ransomware is malware die de databestanden van gebruikers versleutelt, met als doel om deze later te 
+                       ontsleutelen in ruil voor losgeld. In extreme gevallen blokkeert de ransomware de toegang tot het IT-systeem 
+                       door ook systeembestanden te versleutelen die essentieel zijn voor de goede werking van het systeem. 
+                   
+                    <h3>Hoe kan ik het voorkomen?</h3>
+                   <ol>
+                    <ul>•Maak regelmatig back-ups </ul>
+                  	<ul>•Update je software </ul>
+                      </ol>
+                  
+                    </p>
+                    <button typ="button" class="readBtn-wraper"><a href="#" class="read-btn">Read more</a></button>
+                </div>
+                
+                <div class="info-wraper">
+                <div class="info-foto-container">
+                    <img src="./img/scam.jpg" alt="">
+                </div>
+                <div class="info-text">
+                    <h1> DDoS aanvallen </h1>
+                    <p>Bij een DDoS-aanval (Distributed Denial of Service-aanval) krijgt de getroffen server zoveel verzoeken te 
+                        verwerken dat hij het simpelweg niet meer aan kan. De websites zijn dan niet bereikbaar voor bezoekers.
+                   
+                    <h3>Hoe kan ik het voorkomen?</h3>
+                   <ol>
+                    <ul>•Wees er vroeg bij om web verkeer te controleren</ul>
+                  	<ul>•Meer bandbreedte verkrijgen </ul>
+                      </ol>
+                  
+                    </p>
+                    <button typ="button" class="readBtn-wraper"><a href="#" class="read-btn">Read more</a></button>
+                </div>
+                
+                <div class="info-wraper">
+                <div class="info-foto-container">
+                    <img src="./img/scam.jpg" alt="">
+                </div>
+                <div class="info-text">
+                    <h1> Spoofing </h1>
+                    <p>Bij spoofing nemen internetcriminelen een identiteit over. Als zogenaamde bekende of erkende afzender probeert 
+                        de cybercrimineel achter je persoonsgegevens of je pincode te komen, malware op je computer te 
+                        plaatsen of je direct geld afhandig te maken.
+                   
+                    <h3>Hoe kan ik het voorkomen?</h3>
+                   <ol>
+                    <ul>•Stel een google alert in.</ul>
+                  	<ul>•Voorkom email spoofing </ul>
+                      </ol>
+                  
+                    </p>
+                    <button typ="button" class="readBtn-wraper"><a href="#" class="read-btn">Read more</a></button>
+                </div>
+
+                <div class="info-foto-container">
+                    <img src="./img/scam.jpg" alt="">
+                </div>
+
+                <div class="info-text">
+                    <h1> Identiteitsfraude </h1>
+                    <p>Bij identiteitsfraude maakt iemand misbruik van je persoonlijke gegevens. Onder je naam worden er producten of
+                         diensten besteld, uitkeringen of creditcards aangevraagd, betalingen gedaan of bankrekeningen geopend.  
+                   
+                    <h3>Hoe kan ik het voorkomen?</h3>
+                   <ol>
+                    <ul>•Beveilig je computer</ul>
+                  	<ul>•Gebruik sterke wachtwoorden </ul>
+                      </ol>
+                  
+                    </p>
+                    <button typ="button" class="readBtn-wraper"><a href="#" class="read-btn">Read more</a></button>
+                </div>
+                
+                <div class="info-foto-container">
+                    <img src="./img/scam.jpg" alt="">
+                </div>
+
+                <div class="info-text">
+                    <h1> Cryptojacking </h1>
+                    <p> Cryptojacking is een vorm van cybercrime waarbij cybercriminelen geld willen verdienen door cryptogeld in 
+                        handen te krijgen. Ze besmetten de netwerken en apparaten dan bijvoorbeeld met malware.
+                     
+                    <h3>Hoe kan ik het voorkomen?</h3>
+                   <ol>
+                    <ul>•Blokkeer javascript in je browser.</ul>
+                  	<ul>•Gebruik goede antimalware </ul>
+                      </ol>
+                  
+                    </p>
+                    <button typ="button" class="readBtn-wraper"><a href="#" class="read-btn">Read more</a></button>
+                </div>
+                
+                
+                
             </div>
         </div>
 
--- info.php
+++ info.php
@@ -42,9 +42,275 @@ setTimeout(function() {
                 </div>
                 <div class="info-text">
                     <h1> Phishing </h1>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                        Maecenas et iaculis risus. Integer rhoncus, ipsum at congue commodo,
-                        purus velit viverra massa, </p>
+                    <p>Phishing is een vorm van cybercrime waarbij criminelen een e-mail naar je verzenden om te proberen inloggegevens, 
+                        creditcardinformatie, pincodes of andere persoonlijke gegevens van jou te achterhalen. Vandaar het woord phishing, 
+                        dat ‘hengelen’ betekent.Als afzender van de e-mail staat de naam van je bank, creditcardmaatschappij of een 
+                        andere betrouwbare instantie vermeld. In het bericht staat bijvoorbeeld dat de bank je gegevens wil 
+                        controleren vanwege een update of dat je rekening is geblokkeerd. Of je zou een prijs hebben gewonnen of een 
+                        erfenis krijgen. Door de betrouwbaar ogende afzender kun je geneigd zijn te antwoorden op de vraag in de nepmail
+                         om je inloggegevens of pincode. Doe dit nooit. Een bank of andere betrouwbare instantie vraagt niet per e-mail 
+                         om je persoonlijke gegevens aan ze door te geven. Je bank verstuurt alleen e-mail naar je die je kunt vinden 
+                         nadat je bent ingelogd in de app of op de website. Klik ook nooit op een link in de e-mail. Als je dit wel zou 
+                         doen, wordt er ongemerkt op je computer een kwaadaardig programma geïnstalleerd.
+
+
+                    <h3>Hoe kan ik het voorkomen?</h3>
+                   <ol>
+                    <ul>• Reageer niet op het bericht.</ul>
+                  	<ul>• Meld de phishing bij Fraudehelpdesk </ul>
+                    <ul>•Meld de phishing bij de organisatie namens wie het valse bericht verstuurd werd. </ul>
+                    <ul>•	Klik nooit op een link of telefoonnummer als je twijfelt aan de echtheid van een bericht.
+                         Eerst checken dan klikken. </ul>
+                      </ol>
+                  
+                    </p>
+                    <button typ="button" class="readBtn-wraper"><a href="#" class="read-btn">Read more</a></button>
+                </div>
+
+                <div class="intro-info-container">
+            <div class="info-wraper">
+                <div class="info-foto-container">
+                    <img src="./img/spear-phishing.jpeg" alt="">
+                </div>
+                <div class="info-text">
+                    <h1> Scamming </h1>
+                    <p>Elke maand verkopen miljoenen mensen hun soms kostbare spullen via internet aan andere particulieren. 
+                        Meestal gebeurt dit naar volle tevredenheid van alle partijen. Helaas zullen er altijd mensen zijn die de boel
+                         moedwillig oplichten. U betaalt keurig, maar ontvangt vervolgens niets. Of u wordt overgehaald om vast uw 
+                         product op te sturen, maar ontvangt nooit een betaling. In beide gevallen bent u waarschijnlijk opgelicht.
+                          Politie, Openbaar Ministerie, Marktplaats en banken werken samen aan het terugdringen van internetoplichting. 
+
+                    <h3>Hoe kan ik het voorkomen?</h3>
+                   <ol>
+                    <ul>•Doe onderzoek naar de persoon of naar het bedrijf van waar je het koopt.</ul>
+                  	<ul>•Stuur nooit iets op voordat dit ook betaald is. </ul>
+                    <ul>•Als je het niet vertrouwd koop het niet</ul>
+                    <ul>•Betaal niet via een vreemde link. Betaal altijd via de instantie waar het verkocht wordt.</ul>
+                      </ol>
+                  
+                    </p>
+                    <button typ="button" class="readBtn-wraper"><a href="#" class="read-btn">Read more</a></button>
+                </div>
+
+                <div class="intro-info-container">
+            <div class="info-wraper">
+                <div class="info-foto-container">
+                    <img src="./img/spear-phishing.jpeg" alt="">
+                </div>
+                <div class="info-text">
+                    <h1> Malware </h1>
+                    <p>Malware, kort voor 'malicious software' (schadelijke/kwaadaardige software), is een type computerprogramma dat 
+                        is ontworpen om computers te infecteren en er op meerdere manieren schade aan toe te brengen. Malware kan 
+                        computers en apparaten op verschillende manieren infecteren en bestaat in verschillende vormen, zoals virussen, 
+                        wormen, trojans en spyware. Het is essentieel dat alle gebruikers weten hoe malware is te herkennen en hoe ze 
+                        zich tegen alle vormen kunnen beschermen.
+
+                    <h3>Hoe kan ik het voorkomen?</h3>
+                   <ol>
+                    <ul>• Gebruik beveiligingssoftware</ul>
+                  	<ul>• Wees op je hoede </ul>
+                    
+                  
+                    </p>
+                    <button typ="button" class="readBtn-wraper"><a href="#" class="read-btn">Read more</a></button>
+                </div>
+
+                <div class="intro-info-container">
+            <div class="info-wraper">
+                <div class="info-foto-container">
+                    <img src="./img/spear-phishing.jpeg" alt="">
+                </div>
+                <div class="info-text">
+                    <h1> Hacking </h1>
+                    <p>Hacking betekent dat iemand toegang probeert te krijgen tot de gegevens van iemand anders, via het gebruik van computers en het netwerk. 
+                        Het is strafbaar als dit gebeurt zonder toestemming van de eigenaar van die gegevens.
+                        Gegevens stelen kan op verschillende manieren:
+                       <br>•    Door het achterhalen van iemands inloggegevens (door deze te raden of slimme vragen te stellen);
+                       <br>•    Door iemand te dwingen zijn gegevens te vertellen (door bedreiging of chantage);
+                       <br>•    Via phishing.
+                       Bij hacken worden bijvoorbeeld websites of computernetwerken platgelegd of misbruikt. Het is een vorm van cybercrime waarbij computers gebruikt worden om bijvoorbeeld 
+                       gegevens te stelen of om DDos aanvallen uit te voeren.
+
+
+
+                    <h3>Hoe kan ik het voorkomen?</h3>
+                   <ol>
+                    <ul>•Gebruik sterke wachtwoorden</ul>
+                  	<ul>•Meervoudige verificatie gebruiken </ul>
+                    <ul>•Wees waakzaam tegen phishing</ul>
+                    <ul>•Houd ani-virus software up to date.</ul>
+                    <ul>•Vermijd dubieuze websites</ul>
+                    <ul>•Gebruik geen openbare wifi</ul>
+                      </ol>
+                  
+                    </p>
+                    <button typ="button" class="readBtn-wraper"><a href="#" class="read-btn">Read more</a></button>
+                </div>
+
+                <div class="intro-info-container">
+            <div class="info-wraper">
+                <div class="info-foto-container">
+                    <img src="./img/spear-phishing.jpeg" alt="">
+                </div>
+                <div class="info-text">
+                    <h1> Ransomware</h1>
+                    <p>Ransomware is malware die de databestanden van gebruikers versleutelt, met als doel om deze later te ontsleutelen in ruil voor losgeld. 
+                        In extreme gevallen blokkeert de ransomware de toegang tot het IT-systeem door ook systeembestanden te versleutelen die essentieel zijn voor de 
+                        goede werking van het systeem. Gezien het destructieve karakter van ransomware-aanvallen is het vaak moeilijk om de logbestanden te herstellen 
+                        en te achterhalen wat er daadwerkelijk is gebeurd. Hackers kunnen intellectueel eigendom of persoonsgegegevens hebben gestolen, waarbij zij 
+                        ransomware inzetten om hun echte bedoelingen te verbergen.
+
+
+                    <h3>Hoe kan ik het voorkomen?</h3>
+                   <ol>
+                    <ul>•Maak regelmatig back-ups</ul>
+                  	<ul>•Installeer goed anti virus programma </ul>
+                    <ul>•Update je software  </ul>
+                    
+                      </ol>
+                  
+                    </p>
+                    <button typ="button" class="readBtn-wraper"><a href="#" class="read-btn">Read more</a></button>
+                </div>
+
+                <div class="intro-info-container">
+            <div class="info-wraper">
+                <div class="info-foto-container">
+                    <img src="./img/spear-phishing.jpeg" alt="">
+                </div>
+                <div class="info-text">
+                    <h1> DDoS aanvallen </h1>
+                    <p>Bij een DDoS-aanval (Distributed Denial of Service-aanval) krijgt de getroffen server zoveel verzoeken te verwerken dat hij het simpelweg niet meer
+                         aan kan. De websites zijn dan niet bereikbaar voor bezoekers.Om dit voor elkaar te krijgen, controleert de aanvaller een groot aantal computers.
+                          Dit zijn meestal gewoon computers van mensen of bedrijven die de aanvaller heeft geïnfecteerd met schadelijke software, ook wel een ‘botnet’ 
+                          genoemd. Met één druk op de knop is de aanvaller via een botnet in staat om automatisch grote hoeveelheden data te versturen vanaf de 
+                          verschillende locaties. Net zolang tot de server de vraag niet meer aankan en onbereikbaar wordt.
+
+                        
+
+
+                    <h3>Hoe kan ik het voorkomen?</h3>
+                   <ol>
+                    <ul>•Wees er vroeg bij om web verkeer te controleren</ul>
+                  	<ul>•Meer bandbreedte verkrijgen</ul>
+                    <ul>•Gebruik een content distribution center (CDN)</ul>
+                    
+                      </ol>
+
+                      <h3>Wat te doen als ik doelwit ben van een aanval?</h3>
+                   <ol>
+                    <ul>•Zorg dat er defensieve maatregelen worden genomen</ul>
+                  	<ul>•Bel u hostingprovider </ul>
+                    <ul>•Schakel een specialist in.</ul>
+                    
+                      </ol>
+                  
+                    </p>
+                    <button typ="button" class="readBtn-wraper"><a href="#" class="read-btn">Read more</a></button>
+                </div>
+
+                <div class="intro-info-container">
+            <div class="info-wraper">
+                <div class="info-foto-container">
+                    <img src="./img/spear-phishing.jpeg" alt="">
+                </div>
+                <div class="info-text">
+                    <h1> Spoofing </h1>
+                    <p>Bij spoofing nemen internetcriminelen een identiteit over. Als zogenaamde bekende of erkende afzender probeert de cybercrimineel achter je 
+                        persoonsgegevens of je pincode te komen, malware op je computer te plaatsen of je direct geld afhandig te maken. Bankhelpdeskfraude is een veel 
+                        voorkomende manier van spoofing. Je krijgt een telefoontje van de bank en het telefoonnummer is voor jou ook herkenbaar als het nummer van je 
+                        bank.
+
+
+                    <h3>Hoe kan ik het voorkomen?</h3>
+                   <ol>
+                    <ul>•Stel een google alert in.</ul>
+                  	<ul>•Voorkom email spoofing</ul>
+                    
+                  
+                    </p>
+                    <button typ="button" class="readBtn-wraper"><a href="#" class="read-btn">Read more</a></button>
+                </div>
+
+                <div class="intro-info-container">
+            <div class="info-wraper">
+                <div class="info-foto-container">
+                    <img src="./img/spear-phishing.jpeg" alt="">
+                </div>
+                <div class="info-text">
+                    <h1> Identiteitsfraude </h1>
+                    <p>Bij identiteitsfraude maakt iemand misbruik van je persoonlijke gegevens. Onder je naam worden er producten of diensten besteld, uitkeringen of 
+                        creditcards aangevraagd, betalingen gedaan of bankrekeningen geopend. De fraudeur maakt zich schuldig aan oplichting en diefstal. Dit is 
+                        strafbaar. Identiteitsfraude kan ernstige gevolgen hebben.
+
+
+                    <h3>Hoe kan ik het voorkomen?</h3>
+                   <ol>
+                    <ul>•Beveilig je computer</ul>
+                  	<ul>•Gebruik sterke wachtwoorden </ul>
+                    <ul>•Reageer nooit en phishing e-mails </ul>
+                    <ul>•Pas op met het weggeven van informatie over jezelf op het internet.</ul>
+                    <ul>•Ga niet in op telefoontjes waar informatie over jezelf gevraagd wordt</ul>
+                    
+                      </ol>
+                  
+                    </p>
+                    <button typ="button" class="readBtn-wraper"><a href="#" class="read-btn">Read more</a></button>
+                </div>
+
+                <div class="intro-info-container">
+            <div class="info-wraper">
+                <div class="info-foto-container">
+                    <img src="./img/spear-phishing.jpeg" alt="">
+                </div>
+                <div class="info-text">
+                    <h1> Cryptojacking </h1>
+                    <p>Cryptojacking is een vorm van cybercrime waarbij cybercriminelen geld willen verdienen door cryptogeld in handen te krijgen. 
+                        Dit doen ze in te breken op zoveel mogelijk wifi-netwerken, computers en websites, ook van mensen die niet in cryptogeld handelen. 
+                        Ze besmetten de netwerken en apparaten dan bijvoorbeeld met malware om ze onderdeel te maken van een botnet voor cryptomining.
+
+
+                    <h3>Hoe kan ik het voorkomen?</h3>
+                   <ol>
+                    <ul>•Blokkeer javascript in je browser.</ul>
+                  	<ul>•Gebruik goede antimalware </ul>
+                    <ul>•Gebruik een adblocker of anti-cryptomining software  </ul>
+                  
+                      </ol>
+                  
+                    </p>
+                    <button typ="button" class="readBtn-wraper"><a href="#" class="read-btn">Read more</a></button>
+                </div>
+
+                <div class="intro-info-container">
+            <div class="info-wraper">
+                <div class="info-foto-container">
+                    <img src="./img/spear-phishing.jpeg" alt="">
+                </div>
+                <div class="info-text">
+                    <h1> Phishing </h1>
+                    <p>Phishing is een vorm van cybercrime waarbij criminelen een e-mail naar je verzenden om te proberen inloggegevens, 
+                        creditcardinformatie, pincodes of andere persoonlijke gegevens van jou te achterhalen. Vandaar het woord phishing, 
+                        dat ‘hengelen’ betekent.Als afzender van de e-mail staat de naam van je bank, creditcardmaatschappij of een 
+                        andere betrouwbare instantie vermeld. In het bericht staat bijvoorbeeld dat de bank je gegevens wil 
+                        controleren vanwege een update of dat je rekening is geblokkeerd. Of je zou een prijs hebben gewonnen of een 
+                        erfenis krijgen. Door de betrouwbaar ogende afzender kun je geneigd zijn te antwoorden op de vraag in de nepmail
+                         om je inloggegevens of pincode. Doe dit nooit. Een bank of andere betrouwbare instantie vraagt niet per e-mail 
+                         om je persoonlijke gegevens aan ze door te geven. Je bank verstuurt alleen e-mail naar je die je kunt vinden 
+                         nadat je bent ingelogd in de app of op de website. Klik ook nooit op een link in de e-mail. Als je dit wel zou 
+                         doen, wordt er ongemerkt op je computer een kwaadaardig programma geïnstalleerd.
+
+
+                    <h3>Hoe kan ik het voorkomen?</h3>
+                   <ol>
+                    <ul>• Reageer niet op het bericht.</ul>
+                  	<ul>• Meld de phishing bij Fraudehelpdesk </ul>
+                    <ul>•Meld de phishing bij de organisatie namens wie het valse bericht verstuurd werd. </ul>
+                    <ul>•	Klik nooit op een link of telefoonnummer als je twijfelt aan de echtheid van een bericht.
+                         Eerst checken dan klikken. </ul>
+                      </ol>
+                  
+                    </p>
                     <button typ="button" class="readBtn-wraper"><a href="#" class="read-btn">Read more</a></button>
                 </div>
             </div>
